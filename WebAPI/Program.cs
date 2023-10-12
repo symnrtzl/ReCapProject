0e90ece18ffebd4dc4cfd560b464ce5f2f84909b
symnrtzl <symnrtzl99@gmail.com>
2023-10-12 04:00:13 +0300

    CarsController, ColorsController, CustomersController, RentalsController, UsersController

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -15,6 +15,19 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IBrandService, BrandManager>();
 builder.Services.AddSingleton<IBrandDal,EfBrandDal>();
+builder.Services.AddSingleton<ICarService, CarManager>();
+builder.Services.AddSingleton<ICarDal, EfCarDal>();
+builder.Services.AddSingleton<IColorService, ColorManager>();
+builder.Services.AddSingleton<IColorDal, EfColorDal>();
+builder.Services.AddSingleton<ICustomerService, CustomerManager>();
+builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
+builder.Services.AddSingleton<IRentalService, RentalManager>();
+builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
+builder.Services.AddSingleton<IUserService, UserManager>();
+builder.Services.AddSingleton<IUserDal, EfUserDal>();
+
+
+
 
 
 var app = builder.Build();
