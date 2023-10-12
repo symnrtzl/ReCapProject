bfd2116f0a2a1033a267c6a7dc24ae001d8553ff
symnrtzl <symnrtzl99@gmail.com>
2023-10-12 03:25:32 +0300

    BrandsController

---
+++ WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,79 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        IBrandService _brandService;
+
+        public BrandsController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result =_brandService.GetAll();
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        ///api/brands/getbyid?id=1
+        [HttpGet("getbyid")]
+        public IActionResult Get(int id)
+        {
+            var result=_brandService.GetById(id);
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        ///api/brands/add
+        [HttpPost("add")]
+        public IActionResult Post(Brand brand)
+        {
+            var result = _brandService.Add(brand);
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpPost("update")]
+        public IActionResult Put(Brand brand)
+        {
+            var result = _brandService.Update(brand);
+            if(result.Success) 
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(Brand brand) 
+        {
+            var result = _brandService.Delete(brand);
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
