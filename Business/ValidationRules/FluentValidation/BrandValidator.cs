a3c6eecacfb78bc8024844026264f49e4b3ee466
symnrtzl <symnrtzl99@gmail.com>
2023-10-16 22:01:03 +0300

    Brand Validation

---
+++ Business/ValidationRules/FluentValidation/BrandValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class BrandValidator : AbstractValidator<Brand>
+    {
+        public BrandValidator() 
+        {
+            RuleFor(p => p.BrandName).NotEmpty();
+            RuleFor(p => p.BrandName).MinimumLength(2);
+            RuleFor(p => p.BrandName).MaximumLength(20);
+            RuleFor(p => p.BrandModel).NotEmpty();
+        }
+    }
+}
